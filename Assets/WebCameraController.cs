54dc5a1f4420f1aaeb505e21a4d01f55de9c46a2
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-10-31 00:04:44 +0900

    Initial commit

---
+++ Assets/WebCameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCameraController : MonoBehaviour
+{
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+    public Material targetMaterial;
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        // display all cameras
+        for (var i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(devices[i].name);
+        }
+
+        WebCamTexture webcamTexture = new WebCamTexture(devices[0].name, Width, Height, FPS);
+        if (targetMaterial)
+            targetMaterial.mainTexture = webcamTexture;
+        else Debug.LogError("targetMaterial not configured");
+        webcamTexture.Play();
+    }
+}
